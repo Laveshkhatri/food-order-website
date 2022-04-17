00c3a996053a01d1d0229e92fa8bfd1d241bdce8
lavesh <94523498+Laveshkhatri@users.noreply.github.com>
2022-04-17 20:01:51 +0500

    Add files via upload

---
+++ foodorder/french.php
@@ -0,0 +1,44 @@
+<?php
+
+
+$quantity=$_POST['quantity'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$address=$_POST['address'];
+
+
+
+$conn = new mysqli('localhost', 'root', '','foodorder');
+if ($conn->connect_error){
+die('Connect Error :'.$conn->connect_error);
+}
+
+
+ else {
+		$stmt = $conn->prepare("insert into frenchfries(quantity, name, phone, email, address ) values(?, ?, ?, ?, ?)");
+		$stmt->bind_param('isiss', $quantity, $name, $phone, $email, $address);
+		$execval = $stmt->execute();
+
+		
+	    $stmt->close();
+		$conn->close();
+	}
+
+
+
+ ?>
+
+
+
+
+
+
+
+<?php
+
+
+include'index.html'
+
+?>
+
